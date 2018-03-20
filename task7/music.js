e70753b8e4beb72967867fba235f735dc0c8a2d5
Spareaftertaste <2383860945@qq.com>
2018-03-20 22:43:36 +0800

    完成了任务8-9 首页的大部分布局还有CSS

--- task7/music.js
+++ task7/music.js
@@ -1,9 +1,12 @@
 function playmusic() {
     var music = document.getElementById('music');
+    var music_btn = document.getElementById('music_btn2');
     if (music.paused){
         music.play();
+        music_btn.src='img/play.png'
     }
     else{
         music.pause();
+        music_btn.src='img/pause.png'
     }
 }
